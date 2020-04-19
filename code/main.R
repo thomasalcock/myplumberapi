6bd04294e0e4b68f160bcbd66bd87a9e9c8f4e5a
thomasalcock <thomas.alcock@statworx.com>
2020-04-19 13:24:32 +0200

    init

---
+++ code/main.R
@@ -0,0 +1,22 @@
+library(plumber)
+library(dplyr)
+library(janitor)
+df <- readRDS("data/real_estate_data.RDS")
+df <- janitor::clean_names(df)
+
+#' @title Gets data for shiny app
+#' @param min_sq_ft
+#' @param max_price
+#' @get /getdata
+
+function(min_sq_ft, max_price){
+  df %>% 
+    dplyr::filter(
+      lot_area >= min_sq_ft &
+      sale_price <= max_price
+    ) %>% 
+    dplyr::select(
+      lot_area,
+      dplyr::starts_with("sale")
+    )
+}
